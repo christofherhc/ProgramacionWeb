778b10a02e4b7b6bed03af9a9346f4d968fe3a1b
Christofher <christofherhc@hotmail.com>
2024-01-11 09:43:51 -0500

    Tercer Commit

--- 02-ejemplo02.php
+++ 02-ejemplo02.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     
@@ -22,6 +23,8 @@
 
         ?>
     </section>
-
+    <footer>
+        <p>Todos los derechos reservados</p>
+    </footer>
 </body>
 </html>
