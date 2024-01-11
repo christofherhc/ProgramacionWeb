1bbde90e7677cb4bd25f55d7b2f50399c91a15b1
Christofher <christofherhc@hotmail.com>
2024-01-11 09:19:53 -0500

    Mi primer Commit

---
+++ 01-holaMundo.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>01 PHP - Hola Mundo!</title>
+</head>
+<body>
+    
+
+<!-- PHP -->    
+<?php
+    echo("Hola Mundo!");
+?>
+
+<!-- Contenido HTML -->
+
+
+github
+</body>
+</html>c
---
+++ README.md
@@ -0,0 +1,3 @@
+# Repositorio del curso Programacion Web
+
+Aqui se almacenara todos los codigos de las clases realizadas y practicadas en PHP para el curso Programacion Web de la carrera de Ingenieria de Sistemas e Informatica de la UTEA.
