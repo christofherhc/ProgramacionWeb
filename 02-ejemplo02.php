cc410eaa82e045bc34e8dd8a15f9ddec4b9169ed
Christofher <christofherhc@hotmail.com>
2024-01-11 09:40:10 -0500

    Segundo commit

---
+++ 02-ejemplo02.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <header>
+        <h1>Fundamentos de la Programacion</h1>
+    </header>
+
+    <section>
+
+        <!-- PHP -->  
+        <?php
+
+            echo "<br>1. Integrar codigo PHP en HTML5";
+            echo "<br>2. Pagina estatica versus Pagina dinamica";
+            echo "<br>3. Manejo de variables en PHP";
+
+        ?>
+    </section>
+
+</body>
+</html>
