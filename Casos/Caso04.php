4c9562454cda4eb8921de4f594a65f9372fd0159
Christofher <christofherhc@hotmail.com>
2024-01-18 10:05:26 -0500

    Caso04

---
+++ Casos/Caso04.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Salario de Empleados</title>
+    <link rel="stylesheet" href="/estilo.css">
+</head>
+
+<body>
+    <header>
+        <h3>PAGO DE SALARIO DE EMPLEADOS</h3>
+        <img src="banner.jpg" width="600" height="150">
+    </header>
+    <section>
+        
+    </section>
+    <footer>
+        <h6>Todos los derechos reservados Chris</h6>
+    </footer>
+</body>
+
+</html>
